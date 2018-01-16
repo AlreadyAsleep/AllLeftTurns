473bca0451d67add44455636aa1332c7f7a5ccb9
AlreadyAsleep <benheil@ius.edu>
2018-01-16 12:42:21 -0500

    Began writing scripts for agents

---
+++ src/agents.py
@@ -0,0 +1,17 @@
+# File: agents.py
+# Author: Ben Heil
+# Version 1.0
+# Since: 1/16/18
+#
+# This file will contain 3 distinct maze-solving agents that will inherit from the general 'Agent' class:
+# - The first will be the 'AllLeft' agent:
+#       * No memory
+#       * Algorithm goes straight when it can, turns left when forced to turn, and turns around otherwise
+#
+
+
+from src import maze
+
+class Agent:
+
+    def __init__(self):
