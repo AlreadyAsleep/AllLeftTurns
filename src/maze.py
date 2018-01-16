04fd4435044eac36e94d3f542af79e40a3fa2ec8
AlreadyAsleep <benheil@ius.edu>
2018-01-15 23:41:09 -0500

    Began testing environment

--- src/maze.py
+++ src/maze.py
@@ -21,18 +21,18 @@ class Maze:
     def __init__(self, size):
         """Initializes maze to required NxN size"""
         self.size = size
-        self.adjacency = numpy.zeroes(size, size)
+        self.adjacency = numpy.zeros((size, size))
 
     def __str__(self):
         return self.adjacency.__repr__()
 
     def make_maze(self):
         """Populates the maze with obstacles"""
-        self.adjacency[0][random.randint(self.size)] = 1  # creates the entry to the maze
-        self.adjacency[self.size][random.radint(self.size)] = 1  # creates an exit to the maze
-        for i in range(1, self.size - 1):
-            for j in range(self.size):
-                self.adjacency[i][j] = random.randint % 2  # will place a zero if even and a one if not
+        self.adjacency[0][random.randint(0, self.size - 1)] = 1  # creates the entry to the maze
+        self.adjacency[self.size - 1][random.randint(0, self.size - 1)] = 1  # creates an exit to the maze
+        for i in range(1, self.size - 2):
+            for j in range(self.size - 1):
+                self.adjacency[i][j] = random.randint(0, 100) % 2  # will place a zero if even and a one if not
 
 
 
---
+++ src/test.py
@@ -0,0 +1,6 @@
+from src import maze
+m1 = maze.Maze(4)
+m1.make_maze()
+print(m1)
+
+
