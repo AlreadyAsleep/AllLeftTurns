6fede1153f68c7f808c2c30a08cde04b2e833b5c
AlreadyAsleep <benheil@ius.edu>
2018-01-18 14:42:46 -0500

    Implemented ran-gen maze code from Wiki article to generate test cases

--- src/maze.py
+++ src/maze.py
@@ -52,3 +52,62 @@ def gen_from_maze_file(file):
         m.adjacency[count] = current
         count += 1
     return m
+
+
+#
+#   BORROWED AND EDITED CODE FROM : https://en.wikipedia.org/wiki/Maze_generation_algorithm
+#
+#
+from numpy.random import random_integers as rand
+
+
+def maze_generator(width=81, height=51, complexity=.75, density=.75):
+    # Only odd shapes
+    shape = ((height // 2) * 2 + 1, (width // 2) * 2 + 1)
+    # Adjust complexity and density relative to maze size
+    complexity = int(complexity * (5 * (shape[0] + shape[1])))
+    density = int(density * ((shape[0] // 2) * (shape[1] // 2)))
+    # Build actual maze
+    Z = numpy.zeros(shape)
+    # Fill borders
+    Z[0, :] = Z[-1, :] = 1
+    Z[:, 0] = Z[:, -1] = 1
+    # Make aisles
+    for i in range(density):
+        x, y = rand(0, shape[1] // 2) * 2, rand(0, shape[0] // 2) * 2
+        Z[y, x] = 1
+        for j in range(complexity):
+            neighbours = []
+            if x > 1:             neighbours.append((y, x - 2))
+            if x < shape[1] - 2:  neighbours.append((y, x + 2))
+            if y > 1:             neighbours.append((y - 2, x))
+            if y < shape[0] - 2:  neighbours.append((y + 2, x))
+            if len(neighbours):
+                y_,x_ = neighbours[rand(0, len(neighbours) - 1)]
+                if Z[y_, x_] == 0:
+                    Z[y_, x_] = 1
+                    Z[y_ + (y - y_) // 2, x_ + (x - x_) // 2] = 1
+                    x, y = x_, y_
+    # Added code to make compatible with existing implementation
+    # Flip 1's and 0's
+    for i in range(width + 1):
+        for j in range(height + 1):
+            if Z[i][j] == 1:
+                Z[i][j] = 0
+            elif Z[i][j] == 0:
+                Z[i][j] = 1
+
+    # Add entrance
+    ones = []
+    for num in range(len(Z[1])):
+        if Z[1][num] == 1:
+            ones.append(num)
+    Z[0][rand(ones[0], ones[len(ones) - 1])] = 1
+
+    # Add exit
+    ones.clear()
+    for num in range(len(Z[height - 1])):
+        if Z[height - 1][num] == 1:
+            ones.append(num)
+    Z[height][rand(ones[0], ones[len(ones) - 1])] = 1
+    return Z
--- src/test.py
+++ src/test.py
@@ -1,11 +1,22 @@
 from src import maze, agents
-m1 = maze.gen_from_maze_file("../maze_files/10by10.maze")
+'''
+m1 = maze.gen_from_maze_file("../maze_files/4by4.maze")
 print(m1)
 
+
 a1 = agents.LeftOrRight(m1)
-a1.run(m1, 10)
+a1.run(m1, 100)
+
+a2 = agents.AllLeft(m1)
+a2.run(m1)
+'''
+m1 = maze.Maze(26)
+m1.adjacency = maze.maze_generator(m1.size, m1.size, 1, 1)
+print(m1)
 
 a2 = agents.AllLeft(m1)
 a2.run(m1)
 
+a1 = agents.LeftOrRight(m1)
+a1.run(m1, 1)
 
