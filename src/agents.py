c9433cb444a0369099ca97bc290031b90fbd8770
AlreadyAsleep <benheil@ius.edu>
2018-01-17 20:38:46 -0500

    Implemented first goal-based agent name "AllLeft"

--- src/agents.py
+++ src/agents.py
@@ -37,11 +37,11 @@ class AllLeft(Agent):
     # sensors
     direction = ""
     steps = 0
-    location = (0, 0)
+    location = [0, 0]
     end = 0
     active = False
     front = False
-    left  = False
+    left = False
     right = False
 
     # inherited methods
@@ -50,8 +50,8 @@ class AllLeft(Agent):
         self.steps = 1
         for i in range(len(maze.adjacency[0])):
             if maze.adjacency[0][i] == 1:
-                self.location = (0, i)
-        self.end = maze.size
+                self.location = [0, i]
+        self.end = maze.size - 1
         self.active = True
         self.front = True
 
@@ -60,80 +60,107 @@ class AllLeft(Agent):
                "\nLocation: " + str(self.location)
 
     def move(self, direction):
+        self.steps += 1
         if direction == "forward":
             if self.direction == "down":
                 self.location[0] += 1
-            elif self.direciton == "left":
+            elif self.direction == "left":
                 self.location[1] -= 1
             elif self.direction == "right":
                 self.location[1] += 1
             else:
                 self.location[0] -= 1
-        if direction == "leftward":
+        elif direction == "leftward":
             if self.direction == "down":
                 self.location[1] += 1
-            elif self.direciton == "left":
-                self.location[1] -= 1
+                self.direction = "right"
+            elif self.direction == "left":
+                self.location[0] += 1
+                self.direction = "down"
             elif self.direction == "right":
-                self.location[1] += 1
-            else:
                 self.location[0] -= 1
+                self.direction = "up"
+            else:
+                self.location[1] -= 1
+                self.direction = "left"
+        elif direction == "rightward":
+            if self.direction == "down":
+                self.location[1] -= 1
+                self.direction = "left"
+            elif self.direction == "left":
+                self.location[0] -= 1
+                self.direction + "up"
+            elif self.direction == "right":
+                self.location[0] += 1
+                self.direction = "down"
+            else:
+                self.location[1] += 1
+                self.direction = "up"
+        else:  # turn around
+            if self.direction == "down":
+                self.direction = "up"
+            elif self.direction == "left":
+                self.direction = "right"
+            elif self.direction == "right":
+                self.direction = "left"
+            else:
+                self.direction = "down"
 
     def perceive(self, directions):
         if self.location[0] == self.end:
             self.active = False
         elif self.direction == "down":
             if directions[self.location[0] + 1][self.location[1]] == 1:
-                front = True
+                self.front = True
             else:
-                front = False
+                self.front = False
             if directions[self.location[0]][self.location[1] + 1] == 1:
-                left = True
+                self.left = True
             else:
-                left = False
+                self.left = False
             if directions[self.location[0]][self.location[1] - 1] == 1:
-                right = True
+                self.right = True
             else:
-                right = False
+                self.right = False
         elif self.direction == "left":
             if directions[self.location[0]][self.location[1] - 1] == 1:
-                front = True
+                self.front = True
             else:
-                front = False
+                self.front = False
             if directions[self.location[0] + 1][self.location[1]] == 1:
-                left = True
+                self.left = True
             else:
-                left = False
+                self.left = False
             if directions[self.location[0] - 1][self.location[1]] == 1:
-                right = True
+                self.right = True
             else:
-                right = False
+                self.right = False
         elif self.direction == "right":
             if directions[self.location[0]][self.location[1] + 1] == 1:
-                front = True
+                self.front = True
             else:
-                front = False
+                self.front = False
             if directions[self.location[0] - 1][self.location[1]] == 1:
-                left = True
+                self.left = True
             else:
-                left = False
-            if directions[self.location[0] + 1][self.location[1] - 1] == 1:
-                right = True
+                self.left = False
+            if directions[self.location[0] + 1][self.location[1]] == 1:
+                self.right = True
             else:
-                right = False
+                self.right = False
         elif self.direction == "up":
             if directions[self.location[0] - 1][self.location[1]] == 1:
-                front = True
+                self.front = True
             else:
-                front = False
+                self.front = False
             if directions[self.location[0]][self.location[1] - 1] == 1:
-                left = True
+                self.left = True
             else:
-                left = False
+                self.left = False
             if directions[self.location[0]][self.location[1] + 1] == 1:
-                right = True
+                self.right = True
             else:
-                right = False
+                self.right = False
 
     def think(self):
         if self.front:
@@ -144,3 +171,9 @@ class AllLeft(Agent):
             self.move("rightward")
         else:
             self.move("turn around")
+
+    def run(self, maze):
+        while self.active:
+            print(self)
+            self.perceive(maze.adjacency)
+            self.think()
--- src/test.py
+++ src/test.py
@@ -3,6 +3,7 @@ m1 = maze.gen_from_maze_file("../maze_files/4by4.maze")
 print(m1)
 
 a1 = agents.AllLeft(m1)
-print(a1)
+a1.run(m1)
+
 
 
